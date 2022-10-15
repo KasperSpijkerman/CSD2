1595a4834863fb2e093a8c9ea4985932fa290935
Kasper Spijkerman <kasper21@live.nl>
2022-10-15 22:30:42 +0200

    Fixed the playback of multiple samples, Added more UI

--- CSD2a/eindopdracht/Eindopdracht_Kasper_Spijkerman.py
+++ CSD2a/eindopdracht/Eindopdracht_Kasper_Spijkerman.py
@@ -100,7 +100,7 @@ def time_dur_to_ts(time_durations):
     return time_stamps
 
 #function for creating events based on timestamps, sample and name
-def create_events(sample,timestamps,name):
+def create_events(sample,timestamps,name,notedur):
     #dictionary for 1 event
     event_dict = {}
     #empty list for different events
@@ -108,7 +108,7 @@ def create_events(sample,timestamps,name):
     #create different dictionaries and add them to a list
     #creating copies of the event dict otherwise it will just output 1 dict.
     for i in range(len(timestamps)): 
-        event_dict = {"Sample":sample, "Name":name, "Ts": timestamps[i]}
+        event_dict = {"Sample":sample, "Name":name, "Ts": timestamps[i], "Nd:":notedur}
         events_list.append(event_dict.copy())
     return events_list
 
@@ -118,56 +118,95 @@ def create_events(sample,timestamps,name):
 #checking the user input and executing the function
 #To do fix correctinput
 if time_sig == "5/4":
-    note_durations = calculate_durations(num_steps_54,num_notes_54)
-    print(note_durations)
+    note_durations_kick = calculate_durations(num_steps_54,num_notes_54)
+    note_durations_snare = calculate_durations(num_steps_54,1)
+    note_durations_hat = calculate_durations(num_steps_54,2)
+    print(note_durations_kick)
+    print(note_durations_snare)
+    print(note_durations_hat)
 
 if time_sig == "7/8":
-    note_durations = calculate_durations(num_steps_78,num_notes_78)
-    print(note_durations)
+    note_durations_kick = calculate_durations(num_steps_78,num_notes_78)
+    note_durations_snare = calculate_durations(num_steps_78,2)
+    note_durations_hat = calculate_durations(num_steps_78,4)
+    print(note_durations_kick)
+    print(note_durations_snare)
+    print(note_durations_hat)
 
 
+#ui for loop amount
+loop_amount = int(input("Enter the amount of loops: "))
+
+#function for the repeating the seq according to amount of loops
+def loop_dur_list(note_durations,loop_amount,):
+    note_durations_loop = list(note_durations)
+    for i in range(loop_amount):
+        for element in note_durations_loop:
+            note_durations.append(element)
+    #print("Looped list:", note_durations)
+    return note_durations
+
+loop_dur_list(note_durations_kick,loop_amount)
+loop_dur_list(note_durations_snare,loop_amount)
+loop_dur_list(note_durations_hat,loop_amount)
 
 #input for bpm
 correctInput = False
 bpm = 120
 #Check the bpm and see if it is the right value. 
-while (not correctInput):
-    user_bpm = input("Enter a bpm: ")
 
-    # check if we 'received' an empty string
-    if not user_bpm:
-        # empty string --> use default
-        correctInput = True
-    else:
-        try:
-            bpm = float(user_bpm)
+default_bpm = True
+
+keep_default = input("Default Bpm is 120, would you like to keep it? y/n ")
+
+if keep_default == "n":
+    default_bpm = False
+else:
+    default_bpm = True
+
+
+if default_bpm == False:
+    while (not correctInput):
+        user_bpm = input("Enter a bpm: ")
+
+        # check if we 'received' an empty string
+        if not user_bpm:
+            # empty string --> use default
             correctInput = True
-        except:
-            print("Incorrect input - please enter a bpm (or enter nothing - default bpm)")
+        else:
+            try:
+                bpm = float(user_bpm)
+                correctInput = True
+            except:
+                print("Incorrect input - please enter a bpm (or enter nothing - default bpm)")
+
 
 print("Bpm is: ", bpm)
 
 #executing functions for converting values to timestamps
-time_durations = note_dur_to_td(note_durations,bpm)
-print("The Time Durations between the notes are:",time_durations)
-timestamps = time_dur_to_ts(time_durations)
-print("The timestamps are:",timestamps)
+time_durations_kick = note_dur_to_td(note_durations_kick,bpm)
+time_durations_snare = note_dur_to_td(note_durations_snare,bpm)
+time_durations_hat = note_dur_to_td(note_durations_hat,bpm)
+# print("The Time Durations between the notes are:",time_durations)
+timestamps_kick = time_dur_to_ts(time_durations_kick)
+timestamps_snare = time_dur_to_ts(time_durations_snare)
+timestamps_hat = time_dur_to_ts(time_durations_hat)
+# print("The timestamps are:",timestamps)
 
-ts_kick_events = create_events(kick_dict["Instrument"],timestamps,kick_dict["Name"])
+ts_kick_events = create_events(kick_dict["Instrument"],timestamps_kick,kick_dict["Name"],note_durations_kick)
+ts_snare_events = create_events(snare_dict["Instrument"],timestamps_snare,snare_dict["Name"],note_durations_snare)
+ts_hat_events = create_events(hat_dict["Instrument"],timestamps_hat,hat_dict["Name"],note_durations_hat)
 
-#print the events
-print("The Kick events: ",ts_kick_events)
 
 
 #add all the events to a combined list
-all_events_list = ts_kick_events #+ ts_snare_events + ts_hat_events
+all_events_list = ts_kick_events + ts_snare_events + ts_hat_events
 
 #sort the list based on timestamps
-all_sortedevents_list = sorted(all_events_list, key=lambda d: d['Ts']) 
 
 #play the sequence when the user inputs y
 on = False
-switch_seq = input("Play the sequence? y/n:")
+switch_seq = input("Play the sequence? y/n: ")
 if switch_seq == "y":
     on = True
 
@@ -175,24 +214,61 @@ if switch_seq == "n":
     on = False
 
 
-def play_event(event):
-    print(event["Name"])
-    print(event["Ts"])
-    event['Sample'].play()
+def play_event(event,i):
+    event[i]['Sample'].play()
+    print(event[i]["Name"])
+    print(event[i]["Ts"])
+all_sortedevents_list = sorted(all_events_list, key=lambda d: d['Ts']) 
+
+
+
+#additional values
+quarter_note_dur = 60 / bpm
+instr_midi_pitch = {
+    "Kick": 35,
+    "Snare": 38,
+    "Hat": 40,
+}
+
+# create the MIDIfile object, to which we can add notes
+midi_file = MIDIFile(1)
+# set name and tempo
+time_beginning = 0
+midi_file.addTrackName(track, time_beginning, "Beat Sample Track")
+midi_file.addTempo(track, time_beginning, bpm)
+
+def add_events_to_midi(events):
+    for event in events:
+        # transform time (sec) to (qnote)
+        qnote_time = event["Ts"] / quarter_note_dur
+        instr_name = event["Name"]
+        midi_file.addNote(track, channel, instr_midi_pitch[instr_name], qnote_time,
+            event['note_dur'], velocity)
+
 
 # store the current time
 time_start = time.time()
+i = 0
 print("Start ", time_start)
 
+
 #Play sequence according to timestamp and instrument
-while on == True:
-    now = time.time() - time_start
-    for i in range(len(all_sortedevents_list)):
+while True:
+        now = time.time() - time_start
+        #play samples if timestamp is reached
         if now >= all_sortedevents_list[i]["Ts"]:
-            play_event(all_sortedevents_list[i])
-            all_sortedevents_list.pop(0)
-            time.sleep(0.1)
-            # play_event(all_sortedevents_list[i])
+            play_event(all_sortedevents_list,i)
+            if all_sortedevents_list:
+                all_sortedevents_list.pop(0)   
+            else:
+                break
+        #if every sample has played quit.
+        if(i == len(all_sortedevents_list)):
+        
+            break   
+
+    
+
 
 
 
@@ -205,31 +281,9 @@ if store == "Y":
     export_true = True
     #Function for exporting the midi ->
 
+if export_true == True:
+    add_events_to_midi(all_sortedevents_list)
 
-# create the MIDIfile object, to which we can add notes
-midi_file = MIDIFile(1)
-# set name and tempo
-time_beginning = 0
-midi_file.addTrackName(track, time_beginning, "Beat Sample Track")
-midi_file.addTempo(track, time_beginning, bpm)
-
-
-#additional values
-quarter_note_dur = 60 / bpm
-instr_midi_pitch = {
-    "Kick": 35,
-    "Snare": 38,
-    "Hat": 40
-}
-
-
-def add_events_to_midi(events):
-    for event in events:
-        # transform time (sec) to (qnote)
-        qnote_time = event["timestamp"] / quarter_note_dur
-        instr_name = event["instr_name"]
-        midi_file.addNote(track, channel, instr_midi_pitch[instr_name], qnote_time,
-            event['note_dur'], velocity)
 
 with open("events_lists.midi",'wb') as outf:
     midi_file.writeFile(outf)
