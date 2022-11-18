b2df2d23e65351415e1004235bf851e79abce30c
Kasper Spijkerman <kasper21@live.nl>
2022-11-18 10:46:24 +0100

    Added printing ingredientlist

--- CSD2b/Opdracht_1_Classes/class_excersise_2.cpp
+++ CSD2b/Opdracht_1_Classes/class_excersise_2.cpp
@@ -1,31 +1,54 @@
 //main, class, functies, public
 #include <iostream>
-#include <random>
-#include <ctime>
-#include <list>
+#include <array>
+#include <string>
+
 
 class Pizza 
 {
 public:
-    int size = 0;
+    int size;
     std:: string sauce;
-    std::array<std::string, 4> ingredients { "Mushroom", "Salami", "Mozarella", "Pesto" };
-
+    //list with ingedrients
+    std::string ingredients[4] = { "Mushroom", "Salami", "Mozarella", "Pesto" };
+    
     void chooseSize()
     {
         std::cout << "Choose pizza size, Small = 15, Italian = 25, Big is 30: " << std::endl;
         std::cin >> size;
         if (size == 15)
         {
-            std::cout << "Small pizza" << size << std::endl;
+            std::cout << "Small pizza size = " << size << " cm" << std::endl;
         }
-}
+        if (size == 25)
+        {
+            std::cout << "Italian pizza size = " << size << " cm" << std::endl;
+        }
+        if (size == 30)
+        {
+            std::cout << "Big pizza size = " << size << " cm" << std::endl;
+        }
+        else 
+        {
+        std::cout << "Choose pizza size, Small = 15, Italian = 25, Big is 30: " << std::endl;
+        std::cin >> size; 
+        }
+
+    }
+    void printIngredients() 
+    {
+        std::cout << "The List of the ingedrients are: " << std::endl;
+        for (int i = 0; i < 4; i++)
+        std::cout << ingredients[i] << "\n";
+    }
 
 };
 
 
 int main() 
 {
-
+Pizza myPizza;
+myPizza.chooseSize();
+myPizza.printIngredients();
 return 0;
 }
