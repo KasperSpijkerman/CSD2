93d5c10a56258427aa294ab742de561eb3c69347
Kasper Spijkerman <kasper21@live.nl>
2023-06-09 11:07:05 +0200

    cleaned up

--- CSD2d/build/Projects/Midi_Plugins/Midi_Synth/CMakeFiles/MidiSynth.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "MidiSynth_artefacts/libMidiSynth_SharedCode.a"
-)
--- CSD2d/build/Projects/Midi_Plugins/Midi_Synth/CMakeFiles/MidiSynth_AU.dir/__/__/__/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_LV2.mm.o.d
+++
@@ -1,4 +0,0 @@
-Projects/Midi_Plugins/Midi_Synth/CMakeFiles/MidiSynth_AU.dir/__/__/__/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_LV2.mm.o: \
-  /Volumes/SSD_Kasper/HKU/Jaar_2/CSD2/CSD2d/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_LV2.mm \
-  /Volumes/SSD_Kasper/HKU/Jaar_2/CSD2/CSD2d/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_LV2.cpp \
-  /Volumes/SSD_Kasper/HKU/Jaar_2/CSD2/CSD2d/JUCE/modules/juce_audio_plugin_client/LV2/juce_LV2_Client.cpp
